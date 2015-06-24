25abf979c74b73dd1bd2e5c86ab73a2d509f9db1
Charles Zahn <charlesazahn@gmail.com>
2015-06-24 16:30:19 -0400

    Add button to form helper and helper method

--- lib/phase9/form_helper.rb
+++ lib/phase9/form_helper.rb
@@ -7,14 +7,25 @@ module FormHelper
     HTML
   end
 
-  def button_to(text, controller, action, method, route_params = {})
+  def button_to(text, action, method = :get, path_params = {})
     # In Rails:
     # button_to text, {controller: :c, action: :a}, method: :m
     <<-HTML
+      <form class="button_to" action="#{path_for(action, path_params)}" method="#{method}">
+        <button type="submit" value="#{text}">
+      </form>
     HTML
   end
 
   private
+
+  def path_for(action)
+    class_name = self.class.to_s.to_s.underscore.chomp('_controller')
+    action_name = action.to_s
+    path_method = "#{class_name}_#{action_name}_path".to_sym
+    
+    self.send(path_method)
+  end
 
   def h(str)
     str.gsub("<","&lt;").gsub(">","&gt;")
