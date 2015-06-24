4b71a87a1e84e8823430cd3a3443bf0299bb538e
Charles Zahn <charlesazahn@gmail.com>
2015-06-24 11:47:24 -0400

    Add template for button_to method

--- lib/phase9/controller_base.rb
+++ lib/phase9/controller_base.rb
@@ -1,8 +1,10 @@
 require_relative '../phase8/controller_base.rb'
 require_relative 'route_helper'
+require_relative 'form_helper'
 
 module Phase9
   class BaseController < Phase8::BaseController
     include RouteHelper
+    include FormHelper
   end
 end
--- lib/phase9/form_helper.rb
+++ lib/phase9/form_helper.rb
@@ -1,11 +1,17 @@
 module FormHelper
   def link_to(name, url)
+    # In Rails
+    # link_to text, url
     <<-HTML
       <a href="#{url}">#{h(name)}</a>
     HTML
   end
 
-  def button_to
+  def button_to(text, controller, action, method, route_params = {})
+    # In Rails:
+    # button_to text, {controller: :c, action: :a}, method: :m
+    <<-HTML
+    HTML
   end
 
   private
